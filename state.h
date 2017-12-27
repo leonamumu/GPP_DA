063e95c9488a5c28bc8a7f6bf90b5c232a485165
leonamumu <linxiaofeng.whu@gmail.com>
2017-12-27 18:21:47 +0800

    fix(component): fix the index of covstd and the variance of vector_randn_boost
    The program is in a dead loop, maybe caused by some syntax error. I change the index of covstd and
    the variance of vector_randn_boost to fix the bug
    
    fix #60 #91 #93

--- state.h
+++ state.h
@@ -57,7 +57,7 @@ class statevector: public deque<matrix<Tm> >{
 			for(size_t k = 0;k < _nrow;++k)
 			{
 				vector<Tm> rands1(_ncol);
-				vector_randn_boost(rands1, _ncol, 0.5, 0.4, 0.3, 0.7);
+				vector_randn_boost(rands1, _ncol, 0.5, 0.04, 0.3, 0.7);
 				for(size_t j = 0;j < _ncol;++j)
 					(*this)[lag][k][j] = rands1[j]; 
 			}
@@ -88,9 +88,9 @@ class statevector: public deque<matrix<Tm> >{
 				vector<Tm> rands2(_ncol);
 				for(size_t j = 0;j < _nrow;++j)
 				{
-					if(covstd[j][0] > 0.7 || covstd[0][j] < 0.3) 
+					if(covstd[j][0] > 0.7 || covstd[j][0] < 0.3) 
 					{
-						vector_randn_boost(rands2, _ncol, 0.5, 0.4, 0.3, 0.7);
+						vector_randn_boost(rands2, _ncol, 0.5, 0.04, 0.3, 0.7);
 						
 						for(size_t k = 0;k < _ncol;++k)
 							(*this)[0][j][k] = rands2[k];	
